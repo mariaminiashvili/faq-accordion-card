87b67b3e3f17827dd7c98ec86926eb7579cf2987
mariaminiashvili <mari.iniashvili@gmail.com>
2023-04-06 12:19:06 +0400

    add faq-accordion project

---
+++ script.js
@@ -0,0 +1,31 @@
+const button = document.querySelectorAll(".button");
+const answer = document.querySelectorAll(".answer-text");
+const question = document.querySelectorAll(".question");
+const arrow = document.querySelectorAll(".arrow");
+const boxArray = new Array(button.length);
+
+
+function openBox(place){
+    if(boxArray[place] == 1){
+        answer[place].classList.add("answer-text"); 
+        arrow[place].classList.add("arrow");
+        question[place].classList.remove("question");
+        boxArray[place] = 0;
+    }else{
+        answer[place].classList.remove("answer-text");
+        arrow[place].classList.remove("arrow");
+        question[place].classList.add("bold");
+
+        boxArray[place] = 1;
+
+    }
+
+}
+
+
+for ( let i = 0; i < button.length; i++ ){
+    boxArray[i] = 0;
+    button[i].addEventListener("click" , function(){
+        openBox(i);
+    })
+}
